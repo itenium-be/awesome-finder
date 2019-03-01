201aca5decb70d003b10f0794d2b37cd14c5cce8
Laoujin <woutervs@hotmail.com>
2019-03-01 23:28:39 +0100

    cleanup + sass + dir structure

---
+++ src/skeleton/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Content
+        </header>
+      </div>
+    );
+  }
+}
